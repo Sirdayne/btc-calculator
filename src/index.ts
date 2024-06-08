be0c77a474589717e323b2ab704f2481d722207b
Yernar <mussinyernar@gmail.com>
2024-06-09 04:41:44 +0600

    fix

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import Dashboard from "./dashboard/Dashboard.vue";
-
-const useDashboard = {
-    install: function (app: { component: (a: string, b: unknown) => void }) {
-        app.component("Dashboard", Dashboard);
-    },
-};
-
-export { Dashboard, useDashboard };
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import { fileURLToPath, URL } from "node:url";
 
-// export default defineConfig({
-//   plugins: [
-//       vue()
-//   ],
-//   resolve: {
-//     alias: {
-//       "@": fileURLToPath(new URL("./src", import.meta.url)),
-//     },
-//   }
-// })
-
-const path = require('path');
-
-module.exports = defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'AidalaBtcCalculator',
-      fileName: (format) => `aidala-btc-calculator.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    cssInjectedByJsPlugin(),
+    splitVendorChunkPlugin()
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-});
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    cssCodeSplit: false,
+    minify: true,
+    lib: {
+      entry: 'src/main.ts',
+      name: 'BundleJS',
+      fileName: () => `aidala-btc-calculator.js`,
+      formats: ['es']
+    }
+  },
+  define: {
+    'process.env': process.env
+  }
+})
